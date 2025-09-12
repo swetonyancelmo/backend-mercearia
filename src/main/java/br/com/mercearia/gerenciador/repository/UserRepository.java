6bf0bc7fb26acf6199b213b344af08ba898728e6
Swetony <swetonyancelmo@gmail.com>
2025-09-12 10:53:07 -0300

    adicionando a repository de usuarios

---
+++ src/main/java/br/com/mercearia/gerenciador/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.mercearia.gerenciador.repository;
+
+import br.com.mercearia.gerenciador.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
