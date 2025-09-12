40de98e32647fc2ca61ef9e0d21bd3d5b3131941
Swetony <swetonyancelmo@gmail.com>
2025-09-12 10:16:47 -0300

    iniciando projeto

---
+++ src/main/java/br/com/mercearia/gerenciador/GerenciadorMerceariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mercearia.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorMerceariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorMerceariaApplication.class, args);
+	}
+
+}
