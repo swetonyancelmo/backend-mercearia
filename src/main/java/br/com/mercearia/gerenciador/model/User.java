2df11c784c0e2581ce85047da5e61b596a9a83c6
Swetony <swetonyancelmo@gmail.com>
2025-09-12 10:49:19 -0300

    adicionando classe usuario

---
+++ src/main/java/br/com/mercearia/gerenciador/model/User.java
@@ -0,0 +1,27 @@
+package br.com.mercearia.gerenciador.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
