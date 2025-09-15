b8ad4dee2a960a23b3bd80775b3a705a1fb25a2f
Swetony <swetonyancelmo@gmail.com>
2025-09-15 10:12:15 -0300

    adicionando a classe endereço

---
+++ src/main/java/br/com/mercearia/gerenciador/model/Address.java
@@ -0,0 +1,25 @@
+package br.com.mercearia.gerenciador.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private String number;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private String cep;
+}
